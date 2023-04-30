f943bff194ed1ccaf671638dd44b1e0e42ecb01f
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-04-30 13:46:14 +0200

    Update grid.blade.php

--- platform/themes/wowy/partials/shortcodes/sliders/grid.blade.php
+++ platform/themes/wowy/partials/shortcodes/sliders/grid.blade.php
@@ -1,10 +1,11 @@
 <div class="hero-slider-1 dot-style-1 dot-style-1-position-1 {{ $class ?? ''}}" data-autoplay="{{ $shortcode->is_autoplay ?: 'yes' }}" data-autoplay-speed="{{ in_array($shortcode->autoplay_speed, theme_get_autoplay_speed_options()) ? $shortcode->autoplay_speed : 3000 }}">
     @foreach($sliders as $slider)
         <div class="single-hero-slider single-animation-wrap">
-            <div class="container">
-                <div class="single-slider-img single-slider-img-1">
-                    <img class="animated" src="{{ RvMedia::getImageUrl($slider->image, null, false, RvMedia::getDefaultImage()) }}" alt="image">
-                </div>
+            <div class="single-slider-img single-slider-img-1">
+                <img class="animated" src="{{ RvMedia::getImageUrl($slider->image, null, false, RvMedia::getDefaultImage()) }}" alt="image">
+            </div>
+{{--            <div class="container">--}}
+{{--                --}}
 {{--                <div class="row align-items-center slider-animated-1">--}}
 {{--                    <div class="col-lg-5 col-md-6">--}}
 {{--                        {!! Theme::partial('shortcodes.sliders.content', compact('slider')) !!}--}}
@@ -13,7 +14,7 @@
 {{--                        --}}
 {{--                    </div>--}}
 {{--                </div>--}}
-            </div>
+{{--            </div>--}}
         </div>
     @endforeach
 </div>
