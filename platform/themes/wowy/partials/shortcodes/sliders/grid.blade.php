fbf5a007e5b63401386a12b4c006a828011647ae
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-04-30 13:34:37 +0200

    Update grid.blade.php

--- platform/themes/wowy/partials/shortcodes/sliders/grid.blade.php
+++ platform/themes/wowy/partials/shortcodes/sliders/grid.blade.php
@@ -3,10 +3,10 @@
         <div class="single-hero-slider single-animation-wrap">
             <div class="container">
                 <div class="row align-items-center slider-animated-1">
-                    <div class="col-lg-5 col-md-6">
-                        {!! Theme::partial('shortcodes.sliders.content', compact('slider')) !!}
-                    </div>
-                    <div class="col-lg-7 col-md-6">
+{{--                    <div class="col-lg-5 col-md-6">--}}
+{{--                        {!! Theme::partial('shortcodes.sliders.content', compact('slider')) !!}--}}
+{{--                    </div>--}}
+                    <div class="col-lg-12 col-md-6">
                         <div class="single-slider-img single-slider-img-1">
                             <img class="animated" src="{{ RvMedia::getImageUrl($slider->image, null, false, RvMedia::getDefaultImage()) }}" alt="image">
                         </div>
