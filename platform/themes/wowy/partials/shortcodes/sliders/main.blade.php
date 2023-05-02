57f6a49c2b9299f7e64f235b7a36750d05b1c6aa
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-02 14:20:55 +0200

    Update main.blade.php

--- platform/themes/wowy/partials/shortcodes/sliders/main.blade.php
+++ platform/themes/wowy/partials/shortcodes/sliders/main.blade.php
@@ -50,12 +50,12 @@
                 @foreach($sliders as $slider)
                     <div class="single-hero-slider single-animation-wrap">
 {{--                        <div class="container">--}}
-                            <div class="slider-1-height-2 slider-animated-1">
+{{--                            <div class="slider-1-height-2 slider-animated-1">--}}
                                 {!! Theme::partial('shortcodes.sliders.content', compact('slider')) !!}
                                 <div class="single-slider-img single-slider-img-1">
                                     <img src="{{ RvMedia::getImageUrl($slider->image, null, false, RvMedia::getDefaultImage()) }}" alt="image">
                                 </div>
-                            </div>
+{{--                            </div>--}}
 {{--                        </div>--}}
                     </div>
                 @endforeach
