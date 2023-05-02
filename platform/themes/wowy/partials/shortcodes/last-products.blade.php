68debb66c601cb13e5dc5ac90f76776f6664cf8d
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-02 15:33:26 +0200

    dsv

--- platform/themes/wowy/partials/shortcodes/last-products.blade.php
+++ platform/themes/wowy/partials/shortcodes/last-products.blade.php
@@ -3,15 +3,26 @@
 {{--    <product-collections-component title="{!! BaseHelper::clean($title) !!}" :product_collections="{{ json_encode($productCollections) }}" url="{{ route('public.ajax.products') }}"></product-collections-component>--}}
 
 {{--</section>--}}
+@php
+    $products=Botble\Ecommerce\Models\Product::all();
+@endphp
+<div class="row">
 
-<section class="section-padding-60">
-    <div class="container">
-{{--        <h3 class="section-title style-1 mb-30 wow fadeIn animated">{!! BaseHelper::clean($title) !!}</h3>--}}
-        <div class=" owl-carousel owl-theme featured-brands-carousel ">
-
-            <product-collections-component title="{!! BaseHelper::clean($title) !!}" :product_collections="{{ json_encode($productCollections) }}" url="{{ route('public.ajax.products') }}"></product-collections-component>
-
+    @foreach($products as $product)
+        <div class="col-4">
+            @include(Theme::getThemeNamespace() . '::views.ecommerce.includes.product-item', compact('product'))
         </div>
-        {{-- <featured-brands-component url="{{ route('public.ajax.featured-brands') }}"></featured-brands-component> --}}
-    </div>
-</section>
+    @endforeach
+
+</div>
+{{--<section class="section-padding-60">--}}
+{{--    <div class="container">--}}
+{{--        <h3 class="section-title style-1 mb-30 wow fadeIn animated">{!! BaseHelper::clean($title) !!}</h3>--}}
+{{--        <div class=" owl-carousel owl-theme featured-brands-carousel ">--}}
+
+{{--            <product-collections-component title="{!! BaseHelper::clean($title) !!}" :product_collections="{{ json_encode($productCollections) }}" url="{{ route('public.ajax.products') }}"></product-collections-component>--}}
+
+{{--        </div>--}}
+{{--        --}}{{-- <featured-brands-component url="{{ route('public.ajax.featured-brands') }}"></featured-brands-component> --}}
+{{--    </div>--}}
+{{--</section>--}}
