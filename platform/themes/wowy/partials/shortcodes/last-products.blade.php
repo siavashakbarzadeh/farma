063627bf7da0cbb26a460d5eb1ca005175cac8b2
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-02 15:51:04 +0200

    Update last-products.blade.php

--- platform/themes/wowy/partials/shortcodes/last-products.blade.php
+++ platform/themes/wowy/partials/shortcodes/last-products.blade.php
@@ -9,11 +9,11 @@
 
 
 <div class="container mt-5">
-    <h3 class="ps-section__title" style="    color: #103178;
-    font-weight: bold;
-    line-height: 50px;
-    font-size: 30px;text-align: center;
+    <h3 class="ps-section__title" style="
+    font-size: 30px;
+    text-align: center;
     margin-bottom: 20px;
+    margin-top: 20px;
     font-weight: 600;">Gli ultimi prodotti</h3>
 </div>
 <div class="container">
