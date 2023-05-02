a8848400c72703da21881edebe83e89ce3f6dcf1
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-02 12:01:04 +0200

    sdfds

--- platform/themes/wowy/partials/shortcodes/leftadsproduct-admin-config.blade.php
+++ platform/themes/wowy/partials/shortcodes/leftadsproduct-admin-config.blade.php
@@ -14,3 +14,9 @@
         <input type="text" name="subtitle" value="{{ Arr::get($attributes, 'subtitle') }}" class="form-control"
                placeholder="{{ __('Subtitle') }}">
     </div>
+
+
+    <div class="form-group">
+        <label class="control-label">{{ __('Limit') }}</label>
+        <input type="number" name="limit" value="{{ Arr::get($attributes, 'limit') }}" class="form-control" placeholder="{{ __('Limit') }}">
+    </div>
--- platform/themes/wowy/partials/shortcodes/leftadsproduct.blade.php
+++ platform/themes/wowy/partials/shortcodes/leftadsproduct.blade.php
@@ -1,12 +1,27 @@
-<div class="col-lg-4">
-    <div class="banner-img wow fadeIn animated ">
-        <img class="border-radius-10" src="{{ RvMedia::getImageUrl($shortcode->icon) }}" alt="">
-        <div class="banner-text">
-            <span>{!! BaseHelper::clean($shortcode->{'title' }) !!}</span>
-            <h4>{!! BaseHelper::clean($shortcode->{'subtitle'}) !!}</h4>
-{{--            <a href="{{ route('public.ads-click', $ads->key) }}">--}}
-{{--                {{ $ads->getMetaData('button_text', true) ?: __('Shop Now') }} <i class="fa fa-arrow-right"></i>--}}
-{{--            </a>--}}
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="banner-img wow fadeIn animated ">
+                <img class="border-radius-10" src="{{ RvMedia::getImageUrl($shortcode->icon) }}" alt="">
+                <div class="banner-text">
+                    <span>{!! BaseHelper::clean($shortcode->{'title' }) !!}</span>
+                    <h4>{!! BaseHelper::clean($shortcode->{'subtitle'}) !!}</h4>
+                    {{--            <a href="{{ route('public.ads-click', $ads->key) }}">--}}
+                    {{--                {{ $ads->getMetaData('button_text', true) ?: __('Shop Now') }} <i class="fa fa-arrow-right"></i>--}}
+                    {{--            </a>--}}
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-8">
+            <section class="section-padding-60">
+                <div class="container wow fadeIn animated">
+                    @if (clean($title))
+                        <h3 class="section-title style-1 mb-30">{!! BaseHelper::clean($title) !!}</h3>
+                    @endif
+                    <featured-products-component url="{{ route('public.ajax.featured-products', ['limit' => $limit]) }}"></featured-products-component>
+                </div>
+            </section>
         </div>
     </div>
+
 </div>
