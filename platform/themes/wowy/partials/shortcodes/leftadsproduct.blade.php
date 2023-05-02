8ade88dd316537983973010473f7f73c2dd44c54
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-02 13:15:53 +0200

    Update leftadsproduct.blade.php

--- platform/themes/wowy/partials/shortcodes/leftadsproduct.blade.php
+++ platform/themes/wowy/partials/shortcodes/leftadsproduct.blade.php
@@ -19,7 +19,7 @@ $products=Botble\Ecommerce\Models\Product::all();
         <div class="col-lg-8">
             @if ($category)
                 <section class="bg-grey-9 section-padding-60">
-                    <product-category-products-component :category="{{ json_encode($category) }}" :children="{{ json_encode($category->activeChildren) }}" url="{{ route('public.ajax.product-category-products') }}" all="{{ $category->url }}"></product-category-products-component>
+                    <product-category-products-component :category="{{ json_encode($category) }}"  url="{{ route('public.ajax.product-category-products') }}" all="{{ $category->url }}"></product-category-products-component>
                 </section>
             @endif
 {{--            <div class="row">--}}
