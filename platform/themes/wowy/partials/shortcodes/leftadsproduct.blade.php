0c1416a5817a4d1d4e94e35b06f745737aa7ec1b
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-02 11:53:32 +0200

    Update leftadsproduct.blade.php

--- platform/themes/wowy/partials/shortcodes/leftadsproduct.blade.php
+++ platform/themes/wowy/partials/shortcodes/leftadsproduct.blade.php
@@ -2,8 +2,8 @@
     <div class="banner-img wow fadeIn animated ">
         <img class="border-radius-10" src="{{ RvMedia::getImageUrl($shortcode->icon) }}" alt="">
         <div class="banner-text">
-            <span>{!! BaseHelper::clean($shortcode->{'title' . $i}) !!}</span>
-            <h4>{!! BaseHelper::clean($shortcode->{'subtitle' . $i}) !!}</h4>
+            <span>{!! BaseHelper::clean($shortcode->{'title' }) !!}</span>
+            <h4>{!! BaseHelper::clean($shortcode->{'subtitle'}) !!}</h4>
 {{--            <a href="{{ route('public.ads-click', $ads->key) }}">--}}
 {{--                {{ $ads->getMetaData('button_text', true) ?: __('Shop Now') }} <i class="fa fa-arrow-right"></i>--}}
 {{--            </a>--}}
