49f2237a275c75c326c74e732f5c6f55e0987693
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-03 09:55:56 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
 
-            Botble\Base\Middleware\LanguageManager::class,
+           LanguageManager::class,
         ],
 
         'api' => [
