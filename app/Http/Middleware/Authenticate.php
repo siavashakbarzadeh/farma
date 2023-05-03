d08969c291b3d3e26ad08c13a3c69562e810dd0c
siavashakbarzadeh <akbarzadehsiavash@gmail.com>
2023-05-03 10:23:40 +0200

    Update Authenticate.php

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,6 +14,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
+        dd("|");
         if (! $request->expectsJson()) {
             return route('login');
         }
